d66d4ae9bfaa51791ce329df4ce8947f5ad52bc9
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-08-07 00:00:49 +0900

    userモデル単体テストコード用FactoryBotの作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :user do
+    nickname {'furiMa太郎'}
+    email {'sample@gmail.com'}
+    password {'sample01'}
+    password_comfirmation { password }
+    last_name {'田中'}
+    first_name {'太郎'}
+    last_name_kana {'タナカ'}
+    first_name_kana {'タロウ'}
+    birth_date{'1995-01-09'}
     
   end
 end
