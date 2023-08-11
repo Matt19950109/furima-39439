fd364039bdfeef154e292091cd72e20a511caf70
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-08-11 12:37:44 +0900

    itemsコントローラーの設定
    ・itemsコントローラー上でnewとcreateアクションを実施時の設定を追加
    ・データを保存する際のストロングパラメーターを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,22 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except:[:index]
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :item_category_id, :item_status_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
