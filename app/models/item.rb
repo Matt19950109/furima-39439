0b5ca0035c4071c5a3ab0252b4d70dc48aa78c89
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-08-11 11:58:34 +0900

    itemモデルのバリデーションを設定
    ・各カラムのバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,16 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :shipping_days
+
+  #空の投稿を保存できないようにする
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence:true
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
