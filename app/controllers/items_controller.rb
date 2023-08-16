8f5509aa75bf48662689f0595b721946aa2b08b1
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-08-16 21:04:15 +0900

    before_action追加に伴うitemsコントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only:[:show, :edit, :update]
   before_action :authenticate_user!, except:[:index, :show]
   before_action :contributor_confirmation, only:[:edit, :update]
 
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -43,8 +41,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :description, :item_category_id, :item_status_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
 
-  def contributor_confirmation
+  def set_item
     @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
     unless current_user.id == @item.user_id
       redirect_to root_path 
     end
