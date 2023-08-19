93f6462a0bd16f1a6e61baf1ec9d55945cd50feb
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-08-19 12:35:28 +0900

    formオブジェクトパターンにバリデーションを設定
    ・purchasesテーブルに対応するバリデーションを設定
    ・buyersテーブルに対応するバリデーションを設定

--- app/models/purchase_buyer.rb
+++ app/models/purchase_buyer.rb
@@ -2,4 +2,13 @@ class PurchaseBuyer
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
 
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :city
+    validates :house_number
+    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, is invalid. message: "Input only number" }
+    validates :user_id
+    validates :item_id
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 end
