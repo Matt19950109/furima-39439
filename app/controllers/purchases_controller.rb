79eccfb871cd6e4e335a10f7856160412ea31289
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-08-19 16:01:20 +0900

    formオブジェクトパターンのコントローラー設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,24 @@
 class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @purchase_buyer = PurchaseBuyer.new
   end
+
+  def new
+  end
+
+  def create
+    @purchase_buyer = PurchaseBuyer.new(purchase_params)
+    if @purchase_buyer.valid?
+      @purchase_buyer.save
+      redirect_to root_path
+    else
+      render :index, status: :unproccesable_entity
+    end
+  end
+
+  private
+   def purchase_params
+    params.require(:purchase_buyer).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: item_id)
+   end
 end
