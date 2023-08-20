5c43f29daced89f9b1478654496d12043488978a
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-08-21 03:08:02 +0900

    単体テストコード用FactoryBotの作成
    ・formオブジェクトパターンを駆使したモデル(purchase_buyers)の単体テストコード

--- spec/factories/purchase_buyers.rb
+++ spec/factories/purchase_buyers.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :purchase_buyer do
-    
+    postal_code {'123-4567'}
+    prefecture_id {'2'}
+    city {'神奈川県'}
+    house_number {'1-1'}
+    building_name {'神奈川ハイツ'}
+    phone_number {'0123456789'}
+    association :user
+    association :item
   end
 end
