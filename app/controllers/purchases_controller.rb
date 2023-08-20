81260aa36bfc8ba7726b7201cce25fc31ebbaf98
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-08-20 23:28:34 +0900

    formオブジェクトパターンのコントローラ設定の変更
    ・バリデーションに関するエラーメッセージが表示されるように修正
    ・ストロングパラメーターのitem_idカラムを適切な値に修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,16 +9,18 @@ class PurchasesController < ApplicationController
 
   def create
     @purchase_buyer = PurchaseBuyer.new(purchase_params)
+    # binding.pry
     if @purchase_buyer.valid?
       @purchase_buyer.save
       redirect_to root_path
     else
-      render :index, status: :unproccesable_entity
+      @item = Item.find(params[:item_id])
+      render :index, status: :unprocessable_entity
     end
   end
 
   private
    def purchase_params
-    params.require(:purchase_buyer).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: item_id)
+    params.require(:purchase_buyer).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
    end
 end
