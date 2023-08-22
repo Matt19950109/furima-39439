f8f9a7aef130f1c519a9aa16e056835d5dc3c4a6
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-08-23 00:48:59 +0900

    token情報が追加されたことに伴うpurchasesコントローラの編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,7 +9,6 @@ class PurchasesController < ApplicationController
 
   def create
     @purchase_buyer = PurchaseBuyer.new(purchase_params)
-    # binding.pry
     if @purchase_buyer.valid?
       @purchase_buyer.save
       redirect_to root_path
@@ -21,6 +20,7 @@ class PurchasesController < ApplicationController
 
   private
    def purchase_params
-    params.require(:purchase_buyer).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_buyer).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+   end
    end
 end
