20c0877326b0f9f09eec043ee7a517b2908cb8bb
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-08-25 20:48:49 +0900

    商品出品機能のjavaを修正
    ・エラーハンドリング後にjavaが反映されないことを修正

--- app/javascript/break_down.js
+++ app/javascript/break_down.js
@@ -1,10 +1,10 @@
-window.addEventListener('turbo:load', () => {
+window.addEventListener('load', () => {
   const totalPrice = document.getElementById("item-price");
   totalPrice.addEventListener("input", () => {
     const priceValue = totalPrice.value;
     const commission = document.getElementById("add-tax-price");
     const profit = document.getElementById("profit");
     commission.innerHTML = Math.floor(priceValue * 0.1);
-    profit.innerHTML = Math.floor(priceValue - (priceValue * 0.1));
+    profit.innerHTML = Math.ceil(priceValue * 0.9);
   });
 });
